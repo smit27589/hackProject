adf519bfa96a9b646994f1e6c74d2b37ddff2679
Wenjie Chen <wenjie.chen@nyu.edu>
2016-02-24 15:04:06 -0500

    fix: recurring sub type

--- microInvesting/www/js/subscriptionController.js
+++ microInvesting/www/js/subscriptionController.js
@@ -12,7 +12,21 @@ angular.module('starter.controllers').controller('SubscriptionsCtrl', function (
 
   $scope.sub = function () {
     $scope.money.value -= 1;
-
   };
+
+  $scope.subTypes = [
+    {title: 'Weekly', active: true},
+    {title: 'Monthly', active: false},
+    {title: 'Quaterly', active: false}
+  ];
+
+  $scope.clickType = function (type) {
+    $scope.subTypes.forEach(function (itype) {
+      itype.active = false;
+    });
+
+    type.active = true;
+  }
+
 
 });
