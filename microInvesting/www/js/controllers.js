83a074943a993c914af443cc1730e4b22a4982d8
smit27589 <smit27589@gmail.com>
2016-02-24 17:28:53 -0500

    adding InvestmentStartegy Data

--- microInvesting/www/js/controllers.js
+++ microInvesting/www/js/controllers.js
@@ -78,6 +78,90 @@ angular.module('starter.controllers', [])
 
 .controller('SignUpCtrl', function($scope, $state, $ionicSlideBoxDelegate) {
 
+	$scope.categoryPerformanceMap = {'conservative':[{title:'Year to Date',value :'-0.27'},
+	                                                 {title:'6 Month',value :'-1.37'},
+	                                                 {title:'1 Year',value :'-1.33'},
+	                                                 {title:'2 Year',value :'5.66'},
+	                                                 {title:'3 Year',value :'8.27'}],
+	                                 'moderate':[{title:'Year to Date',value :'-1.79'},
+	            	                                                 {title:'6 Month',value :'-3.78'},
+	            	                                                 {title:'1 Year',value :'-2.36'},
+	            	                                                 {title:'2 Year',value :'4.82'},
+	            	                                                 {title:'3 Year',value :'10.53'}],
+	            	                 'growth':[{title:'Year to Date',value :'-3.29'},
+	            	            	                                                 {title:'6 Month',value :'-6.10'},
+	            	            	                                                 {title:'1 Year',value :'-3.37'},
+	            	            	                                                 {title:'2 Year',value :'4.02'},
+	            	            	                                                 {title:'3 Year',value :'12.83'}],
+	            	            	 'aggresive':[{title:'Year to Date',value :'-4.72'},
+	            	            	            	                                                 {title:'6 Month',value :'-8.29'},
+	            	            	            	                                                 {title:'1 Year',value :'-4.36'},
+	            	            	            	                                                 {title:'2 Year',value :'3.22'},
+	            	            	            	                                                 {title:'3 Year',value :'15.28'}]};
+	
+	$scope.compositeMap = {'conservative':[{title:'Fixed Income',value :'80.42'},
+	                                                 {title:'Financials',value :'3.88'},
+	                                                 {title:'Info Tech',value :'3.23'},
+	                                                 {title:'Healthcare',value :'2.70'},
+	                                                 {title:'Industrials',value :'2.24'},
+	                                                 {title:'Consumer Disc',value :'2.23'},
+	                                                 {title:'Consumer Staples',value :'1.34'},
+	                                                 {title:'Energy',value :'1.25'},
+	                                                 {title:'Materials',value :'0.92'},
+	                                                 {title:'Telecom',value :'0.63'},
+	                                                 {title:'Utilities',value :'0.56'}],
+	                                 'moderate':[{title:'Fixed Income',value :'60.53'},
+                                                 {title:'Financials',value :'8.27'},
+                                                 {title:'Info Tech',value :'6.71'},
+                                                 {title:'Healthcare',value :'5.44'},
+                                                 {title:'Industrials',value :'4.19'},
+                                                 {title:'Consumer Disc',value :'4.58'},
+                                                 {title:'Consumer Staples',value :'2.85'},
+                                                 {title:'Energy',value :'2.73'},
+                                                 {title:'Materials',value :'1.74'},
+                                                 {title:'Telecom',value :'1.38'},
+                                                 {title:'Utilities',value :'1.25'}],
+	            	                 'growth':[{title:'Fixed Income',value :'40.41'},
+                                               {title:'Financials',value :'12.81'},
+                                               {title:'Info Tech',value :'8.67'},
+                                               {title:'Healthcare',value :'7.83'},
+                                               {title:'Industrials',value :'6.12'},
+                                               {title:'Consumer Disc',value :'6.54'},
+                                               {title:'Consumer Staples',value :'1.34'},
+                                               {title:'Energy',value :'4.04'},
+                                               {title:'Materials',value :'2.55'},
+                                               {title:'Telecom',value :'2.19'},
+                                               {title:'Utilities',value :'1.92'}],
+	            	            	 'aggresive':[{title:'Fixed Income',value :'15.93'},
+	                                                 {title:'Financials',value :'17.93'},
+	                                                 {title:'Info Tech',value :'11.29'},
+	                                                 {title:'Healthcare',value :'10.69'},
+	                                                 {title:'Industrials',value :'9.14'},
+	                                                 {title:'Consumer Disc',value :'9.40'},
+	                                                 {title:'Consumer Staples',value :'5.87'},
+	                                                 {title:'Energy',value :'4.82'},
+	                                                 {title:'Materials',value :'3.80'},
+	                                                 {title:'Telecom',value :'2.68'},
+	                                                 {title:'Utilities',value :'2.39'}]};
+	
+	
+	$scope.selectedCategory = "conservative";
+	$scope.setSelected = function(type) {
+        $scope.selectedCategory = type;
+    };
+    $scope.isSelected = function(type) {
+        return type === $scope.selectedCategory;
+    };
+	
+	
+	$scope.active = 'performance';
+    $scope.setActive = function(type) {
+        $scope.active = type;
+    };
+    $scope.isActive = function(type) {
+        return type === $scope.active;
+    };
+	
   // need to change it to agreement page
   $scope.startApp = function() {
     $state.go('app.summary');
