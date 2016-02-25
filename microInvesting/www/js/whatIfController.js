7510d97ea9616a363bd2478b3bc1152973ae5e6e
smit27589 <smit27589@gmail.com>
2016-02-25 10:40:09 -0500

    adding what if mode

--- microInvesting/www/js/whatIfController.js
+++ microInvesting/www/js/whatIfController.js
@@ -1,16 +1,30 @@
 angular.module('starter.controllers').controller('WhatIfCtrl', function($scope, $stateParams,$state) {
 
-	$scope.transactionData = [{description:'Whole Foods, Inc',amount:'$33.25',spareChange:'$0.75'},
-	                          {description:'Sunac Natural Food',amount:'$13.85',spareChange:'$0.15'},
-	                          {description:'Subway',amount:'$24.45',spareChange:'$0.55'},
-	                          {description:'Amazon,Inc',amount:'$33.25',spareChange:'$0.75'},
-	                          {description:'Zara',amount:'$133.03',spareChange:'$0.97'},
-	                          {description:'Subscription Service',amount:'$100',spareChange:'$100'},
-	                          {description:'Dunkin Donuts',amount:'$33.25',spareChange:'$0.75'},
-	                          {description:'Harry Pizzeria',amount:'$33.25',spareChange:'$0.75'},
-	                          {description:'Kati Roll Company',amount:'$11.89',spareChange:'$0.11'},
-	                          {description:'Sprint',amount:'$81.10',spareChange:'$0.90'},
-	                          {description:'Whole Foods, Inc',amount:'$123.36',spareChange:'$0.64'}
-	                          ];
+	$scope.conservative = 8.27;
+	$scope.moderate = 10.53;
+	$scope.growth = 12.83;
+	$scope.aggresive = 15.28;
+	$scope.data={age:26,monthly:30,conservative:40,moderate:20,growth:10,aggresive:30};
+	
+	var calculateAmount = function(totalAmount){
+		var expectedValue = (((totalAmount + ((totalAmount*$scope.conservative)/100)))*($scope.data.conservative/100)) +
+		(((totalAmount + ((totalAmount*$scope.moderate)/100))) * ($scope.data.moderate/100)) +
+		(((totalAmount + ((totalAmount*$scope.growth)/100))) * ($scope.data.growth/100)) +
+		(((totalAmount + ((totalAmount*$scope.aggresive)/100))) * ($scope.data.aggresive/100));
+		return expectedValue;
+	};
+	
+	$scope.go = function(){
+		var threeYearAmount = $scope.data.monthly* 36;
+		var dataMap = {};
+		dataMap[$scope.data.age] = 0;
+		var initAmount = 0;
+		for(var i = $scope.data.age + 3; i < 67; i+=3){
+			var initAmount = calculateAmount(initAmount + threeYearAmount);
+			dataMap[i] = initAmount;
+			
+		}
+		console.log(dataMap);
+	};
 
 });
