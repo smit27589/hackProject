5b3325882c540a83b2dd92d1d6c335dbb5271cb1
Vishal Shah <vishal.shah@nyu.edu>
2016-03-09 23:35:36 -0500

    Splash screen

--- microInvesting/www/js/app.js
+++ microInvesting/www/js/app.js
@@ -8,6 +8,9 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','n
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+	  setTimeout(function() {
+		  navigator.splashscreen.hide();
+	    }, 2000);
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
 //    if (window.cordova && window.cordova.plugins.Keyboard) {
