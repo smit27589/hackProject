d376dbe286bbd0807bbe3ac00c993102e53544c3
Wenjie Chen <wenjie.chen@nyu.edu>
2016-03-10 10:54:42 -0500

    some change

--- microInvesting/www/js/app.js
+++ microInvesting/www/js/app.js
@@ -10,7 +10,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','n
   $ionicPlatform.ready(function() {
 	  setTimeout(function() {
 		  navigator.splashscreen.hide();
-	    }, 2000);
+	    }, 5000);
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
 //    if (window.cordova && window.cordova.plugins.Keyboard) {
